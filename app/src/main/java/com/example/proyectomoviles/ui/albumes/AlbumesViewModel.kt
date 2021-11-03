600c057a6e6e5a9cf147fef6853a8dcdbfcc147e
ps-lizarazo <ps.lizarazo@uniandes.edu.co>
2021-11-03 00:53:37 -0500

    Feature/consultar catalogo albumes (#7)
    * agregando pantalla de listado de artistas
    
    * agregando pantalla de listado de artistas
    
    * agregando gitignore
    
    * quitando elementos .idea
    
    * Estilo agregado para coindicir con colores App y caparazon fragmento albumes agregado
    
    * Se agregó visibilidad del texto en la barra siempre
    
    * * Refactor carpetas
    * RecyclerView Catalogo de Albumes added
    
    Co-authored-by: Andres Lopez <am.lopezr1@uniandes.edu.co>
    Co-authored-by: Marby Leguizamón <m.leguizamong@uniandes.edu.co>

--- app/src/main/java/com/example/proyectomoviles/ui/albumes/AlbumesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.proyectomoviles.ui.albumes
-
-import androidx.lifecycle.ViewModel
-
-class AlbumesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/proyectomoviles/viewmodels/ArtistViewModel.kt
+++ app/src/main/java/com/example/proyectomoviles/viewmodels/ArtistViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.proyectomoviles.ui.artist
+package com.example.proyectomoviles.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
